60bdad071640f917bb29d6985865b2c3b3799b0f
Fellipe <fellipe.souza@unifal-mg.edu.br>
2023-12-06 13:34:16 -0300

    feat: Adding a new controller to demonstrate how an API behaves

--- src/main/java/br/com/unifalmg/blog/service/UserService.java
+++ src/main/java/br/com/unifalmg/blog/service/UserService.java
@@ -1,5 +1,6 @@
 package br.com.unifalmg.blog.service;
 
+import br.com.unifalmg.blog.controller.request.UserRequest;
 import br.com.unifalmg.blog.entity.User;
 import br.com.unifalmg.blog.exception.InvalidUserException;
 import br.com.unifalmg.blog.exception.UserNotFoundException;
@@ -37,6 +38,21 @@ public class UserService {
             throw new InvalidUserException();
         }
         return repository.save(user);
+    }
+
+    public void deleteById(Integer id) {
+        repository.deleteById(id);
+    }
+
+    public User add(UserRequest request) {
+        User user = User.builder()
+                .name(request.getName())
+                .username(request.getUsername())
+                .email(request.getEmail())
+                .phone(request.getPhone())
+                .website(request.getWebsite())
+                .build();
+        return add(user);
     }
 
 }
